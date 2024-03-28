aaf42d07730e8ec6c61f10e87b521216fec0e275
iremaltnz <iremaltnz@gmail.com>
2024-03-29 01:59:41 +0300

    Delayed Jobs

---
+++ Hangfire.Web/BackgroundJobs/DelayedJobs.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+namespace Hangfire.Web.BackgroundJobs
+{
+    public class DelayedJobs
+    {
+        public static string AddWaterMarkJob(string filename,string watermarkText)
+        {
+           return Hangfire.BackgroundJob.Schedule(()=> ApplyWatermark(filename,watermarkText),TimeSpan.FromSeconds(30));
+        }
+        public static void ApplyWatermark(string filename, string watermarkText)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pictures", filename);
+
+            using (var bitmap = Bitmap.FromFile(path))
+            {
+                using (Bitmap tempBitmap = new Bitmap(bitmap.Width, bitmap.Height))
+                {
+                    using (Graphics grp = Graphics.FromImage(tempBitmap))
+                    {
+                        grp.DrawImage(bitmap, 0, 0);
+
+                        var font = new Font(FontFamily.GenericSansSerif, 25, FontStyle.Bold);
+
+                        var color = Color.FromArgb(255, 0, 0);
+
+                        var brush = new SolidBrush(color);
+
+                        var point = new Point(20, bitmap.Height - 50);
+
+                        grp.DrawString(watermarkText, font, brush, point);
+
+                        tempBitmap.Save(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pictures/watermarks", filename));
+                    }
+                }
+            }
+        }
+    }
+}
--- Hangfire.Web/Controllers/HomeController.cs
+++ Hangfire.Web/Controllers/HomeController.cs
@@ -30,6 +30,35 @@ namespace Hangfire.Web.Controllers
             
             return View();
         }
+
+        public IActionResult PictureSave()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> PictureSave(IFormFile picture)
+        {
+            string newFileName=String.Empty;
+
+            if (picture != null && picture.Length > 0)
+            {
+                newFileName = Guid.NewGuid().ToString() + Path.GetExtension(picture.FileName);
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pictures", newFileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await picture.CopyToAsync(stream);
+                }
+
+                string jobID = BackgroundJobs.DelayedJobs.AddWaterMarkJob(newFileName, "www.mysite.com");
+
+            }
+
+            return View();
+        }
     }
 
    
